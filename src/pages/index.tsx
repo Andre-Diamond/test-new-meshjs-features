4e3d539a69ad03f1564c143142f89ff2556f2808
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-09-13 11:37:01 +0200

    Initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { CardanoWallet, MeshBadge } from "@meshsdk/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Mesh App on Cardano</title>
+        <meta name="description" content="A Cardano dApp powered my Mesh" />
+      </Head>
+      <main>
+        <h1>
+          <a href="https://meshjs.dev/">
+            Mesh
+          </a>{" "}
+          Next.js
+        </h1>
+
+        <div>
+          <CardanoWallet />
+        </div>
+
+      </main>
+      <footer>
+        <MeshBadge isDark={true} />
+      </footer>
+    </div>
+  );
+}
